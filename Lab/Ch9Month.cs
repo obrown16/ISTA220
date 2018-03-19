bdfcba15d61283b03b7d9485933b1097272be91c
Oscar Brown <oscar.t.brown@outlook.com>
2018-03-19 10:20:38 -0400

    seventh commit

---
+++ Lab/Ch9Month.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StructsAndEnums
+{
+   //to do
+    enum Month
+    {
+        January, February, March, April,
+        May, June, July, August,
+        September, October, November, December
+    }
+}

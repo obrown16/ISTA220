06517d7a55c894f06d89a8b7d60be03b14b2e844
Oscar Brown <oscar.t.brown@outlook.com>
2018-04-24 23:56:54 -0400

    Update 424_6

---
+++ Exercises/Ex06_MilitaryUnit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_6_MilitaryUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //build a Tank crew within a tank platoon
+            Driver Dan = new Driver();
+            Gunner John = new Gunner();
+            TankCdr Oscar = new TankCdr();
+            Loader Matt = new Matt();
+
+            Console.WriteLine("Hello and welcome");
+            Console.WriteLine("I'll let the crew introduce themselves");
+            Console.WriteLine();
+
+            //Call the crew to talk
+            Dan.Talk();
+            John.Talk();
+            Oscar.Talk();
+            Matt.Talk();
+        
+        }
+    }
+}
